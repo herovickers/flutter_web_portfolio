c805f28d625765199ef27fbb7ac9cb903000a8ed
herovickers <victoreronmosele@gmail.com>
2019-09-20 22:50:43 +0100

    Add email functionality

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -248,7 +248,11 @@ class _EmailButtonState extends State<EmailButton> {
       child: FlatButton(
         color: AppColors.WHITE,
         hoverColor: AppColors.PRIMARY,
-        onPressed: () {},
+        onPressed: () {
+          html.window.open(
+              'mailto: enderwiggins003@gmail.com?subject=Hello!&body=Hi,%20Victor',
+              '_top');
+        },
         padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
         child: Text(
           AppStrings.EMAIL,
@@ -307,7 +311,7 @@ class Socials extends StatelessWidget {
             ),
           ),
           onPressed: () {
-            html.window.open(url, 'tab');
+            html.window.open(url, '_blank');
           },
         );
       }),
